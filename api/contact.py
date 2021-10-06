55dbf75c0d26dd37de17fcff1c1d3441867d3727
alexbaico <alexbaico@gmail.com>
2021-10-06 19:32:28 -0300

    added contact file/endpoint

---
+++ api/contact.py
@@ -0,0 +1,15 @@
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write("api = "+os.environ.get('SENDGRID_API_KEY').encode())
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        return
