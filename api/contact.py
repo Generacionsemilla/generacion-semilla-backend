5fdc4e4e572d1ab9cda11c4e26733369e307784e
alexbaico <alexbaico@gmail.com>
2021-10-11 17:25:05 -0300

    migrated to Gmail API

--- api/contact.py
+++ api/contact.py
@@ -1,19 +1,30 @@
-# using SendGrid's Python Library
-# https://github.com/sendgrid/sendgrid-python
 import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
 from http.server import BaseHTTPRequestHandler
+
+from gmail import service_account_login, create_message, send_message
+
+# Email variables. Modify this!
+EMAIL_FROM = os.environ.get('EMAIL_FROM')
+EMAIL_TO = os.environ.get('EMAIL_TO')
+EMAIL_SUBJECT = 'Contacto - Nuevo mensaje desde el sitio de {from_name} <{from_email}>'
+EMAIL_CONTENT = 'Hello, this is a test siqtal siqtal'
+SEND_EMAIL = os.environ.get('EMAIL_SEND_ENABLED')
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        message = "api = "+os.environ.get('SENDGRID_API_KEY')
-        self.wfile.write(message.encode())
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-        print(os.environ)
-        print(os.environ.get('SENDGRID_API_KEY'))
+        """Send with gmail api"""
 
+        from_name_test = 'test'
+        from_email_test = 'test@hmm.com'
+
+        print(self)
+
+        service = service_account_login()
+        # Call the Gmail API
+        message = create_message(from_email_test, EMAIL_TO, EMAIL_SUBJECT.format(from_name=from_name_test, from_email=from_email_test), EMAIL_CONTENT)
+        if SEND_EMAIL:
+            sent = send_message(service, 'me', message)
+            print("Email sent!")
+        else:
+            print("SEND flag is disabled, email not sent")
         return
---
+++ gmail.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+
+import base64
+import os
+from email.mime.text import MIMEText
+
+from apiclient import errors
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+    Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+    Returns:
+    An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+def send_message(service, user_id, message):
+    """Send an email message.
+    Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+    Returns:
+    Sent Message.
+    """
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+def service_account_login():
+    SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('keys.json'):
+        creds = Credentials.from_authorized_user_file('keys.json', SCOPES)
+    if not creds:
+        info = {
+        "refresh_token": os.environ.get('GMAIL_REFRESH_TOKEN'),
+        "client_id": os.environ.get('GMAIL_CLIENT_ID'),
+        "client_secret": os.environ.get('GMAIL_CLIENT_SECRET')
+        }
+        creds = Credentials.from_authorized_user_info(info, SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('keys.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    return service
