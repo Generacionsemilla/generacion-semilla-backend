d415f34315d0a96c7ecb357e00b1a73307d92e27
alexbaico <alexbaico@gmail.com>
2021-10-16 17:04:25 -0300

    send email with info received on post request

--- api/contact.py
+++ api/contact.py
@@ -1,37 +1,53 @@
+import json
 import os
 from http.server import BaseHTTPRequestHandler
 
 from gmail import service_account_login, create_message, send_message
 
+from http.server import BaseHTTPRequestHandler,HTTPServer
+
+#PORT_NUMBER = 8080
+
 # Email variables. Modify this!
-EMAIL_FROM = os.environ.get('EMAIL_FROM')
+#EMAIL_FROM = os.environ.get('EMAIL_FROM')
 EMAIL_TO = os.environ.get('EMAIL_TO')
 EMAIL_SUBJECT = 'Contacto - Nuevo mensaje desde el sitio de {from_name} <{from_email}>'
-EMAIL_CONTENT = 'Hello, this is a test siqtal siqtal'
+EMAIL_CONTENT = 'Te contactaron del sitio con el siguiente mensaje: {message}'
 SEND_EMAIL = os.environ.get('EMAIL_SEND_ENABLED')
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        """Send with gmail api"""
 
-        from_name_test = 'test'
-        from_email_test = 'test@hmm.com'
+    def do_POST(self):
+        content = self.headers.get('Content-Length')
+        data = None
 
-        print(self)
+        if content:
+            content_len = int(content)
+            body = self.rfile.read(content_len)
+            print(body)
+            data = json.loads(body)
+            print(data)
+            print(data['from_name'])
+            print(data['from_email'])
+            print(data['message'])
 
-        service = service_account_login()
-        # Call the Gmail API
-        message = create_message(from_email_test, EMAIL_TO, EMAIL_SUBJECT.format(from_name=from_name_test, from_email=from_email_test), EMAIL_CONTENT)
-        if SEND_EMAIL.lower() == 'true':
-            sent = send_message(service, 'me', message)
-            print("Email sent!")
 
-            #TODO errors handling pending
+        if SEND_EMAIL and 'true' == SEND_EMAIL.lower():
+            if data:
+                # Call the Gmail API
+                service = service_account_login()
+                message = create_message(data['from_email'], EMAIL_TO, EMAIL_SUBJECT.format(from_name=data['from_name'], from_email=data['from_email']), EMAIL_CONTENT.format(message=data['message']))
+                sent = send_message(service, 'me', message)
 
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write("Email sent!".encode())
+                #TODO errors handling pending
+
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write("Email sent!".encode())
+            else:
+                print("No data")
+
         else:
             print("SEND flag is disabled, email not sent")
             self.send_response(200)
@@ -39,3 +55,20 @@ class handler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write("Email send is disabled".encode())
         return
+
+#def run(server_class=HTTPServer, handler_class=handler, port=8080):
+#    server_address = ('', port)
+#    httpd = server_class(server_address, handler_class)
+#    try:
+#        httpd.serve_forever()
+#    except KeyboardInterrupt:
+#        pass
+#    httpd.server_close()
+#
+#if __name__ == '__main__':
+#    from sys import argv
+#
+#    if len(argv) == 2:
+#        run(port=int(argv[1]))
+#    else:
+#        run()
