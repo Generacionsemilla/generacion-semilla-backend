d1c0c41d4b9e88323b8504ae9ab48a5a71652d3c
alexbaico <alexbaico@gmail.com>
2021-10-16 17:33:06 -0300

    cors?3

--- api/contact.py
+++ api/contact.py
@@ -19,10 +19,11 @@ class handler(BaseHTTPRequestHandler):
 
     def do_OPTIONS(self):
         self.send_response(200, "ok")
-        self.send_header('Access-Control-Allow-Credentials', 'true')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-type")
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
 
     def do_POST(self):
         content = self.headers.get('Content-Length')
